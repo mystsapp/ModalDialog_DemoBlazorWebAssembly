d646bc156f2c1aab1b19c2005f3ec4b096b4a9f8
Hong Van <mystsapp@gmail.com>
2021-09-21 16:14:30 +0700

    1

---
+++ ModalDialog/Models/WindowSize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModalDialog.Models
+{
+    public class WindowSize
+    {
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+    }
+}
