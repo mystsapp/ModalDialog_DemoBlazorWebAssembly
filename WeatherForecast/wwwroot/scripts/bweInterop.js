a70d4e0fd4414409d1af7e684c715a6e8dec60dc
Hong Van <mystsapp@gmail.com>
2021-09-22 15:48:53 +0700

    WeatherForecast

---
+++ WeatherForecast/wwwroot/scripts/bweInterop.js
@@ -0,0 +1,21 @@
+﻿var bweInterop = {};
+
+bweInterop.getPosition = async function () {
+    function getPositionAsync() {
+        return new Promise((success, error) => {
+            navigator.geolocation.getCurrentPosition(success, error); // (success, error) : true, false
+        });
+    }
+
+    if (navigator.geolocation) {
+        var position = await getPositionAsync();
+        var coords = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+
+        return coords;
+    } else {
+        throw Error("Geolaction is not supported by this browser.");
+    }
+}
