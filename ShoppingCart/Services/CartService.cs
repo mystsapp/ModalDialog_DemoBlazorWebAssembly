323006802ece6d5d70796e7b5017cdc715568969
Hong Van <mystsapp@gmail.com>
2021-09-24 11:31:34 +0700

    1

---
+++ ShoppingCart/Services/CartService.cs
@@ -0,0 +1,38 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Services
+{
+    public class CartService : ICartService
+    {
+        public CartService()
+        {
+            Cart = new List<Product>();
+        }
+
+        public IList<Product> Cart { get; private set; }
+
+        public int Total { get; set; }
+
+        public event Action OnChange; // OnChange dung de giu trang thai ==> AppState -> for shoping cart's state
+
+        public void AddProduct(Product product)
+        {
+            Cart.Add(product);
+            Total += product.Price;
+            NotifyStateChanged(); // to invoke OnChang()
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            Cart.Remove(product);
+            Total -= product.Price;
+            NotifyStateChanged(); // to invoke OnChang()
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
---
+++ ShoppingCart/Services/ICartService.cs
@@ -0,0 +1,20 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Services
+{
+    internal interface ICartService
+    {
+        IList<Product> Cart { get; }
+        int Total { get; set; }
+
+        event Action OnChange;
+
+        void AddProduct(Product product);
+
+        void DeleteProduct(Product product);
+    }
+}
