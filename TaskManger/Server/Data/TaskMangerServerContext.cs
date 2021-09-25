6c226f85b78f980be9473caa95974e313de1c4ff
Hong Van <mystsapp@gmail.com>
2021-09-25 15:36:05 +0700

    1

---
+++ TaskManger/Server/Data/TaskMangerServerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskManger.Shared;
+
+namespace TaskManger.Server.Data
+{
+    public class TaskMangerServerContext : DbContext
+    {
+        public TaskMangerServerContext (DbContextOptions<TaskMangerServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskManger.Shared.TaskItem> TaskItem { get; set; }
+    }
+}
